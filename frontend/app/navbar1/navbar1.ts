073b5254d93a6c0e7788cf794a662151ad74649b
Miguel A. Angulo <maangulo12@gmail.com>
2017-03-06 19:09:48 -0600

    Update navbar component

---
+++ frontend/app/navbar1/navbar1.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'navbar1-component',
+  templateUrl: 'navbar1.html'
+})
+export class Navbar1Component { 
+
+}
