252d3d2e7a5eb7458491b1f162c3f0c1d2b90ba4
Miguel A. Angulo <maangulo12@gmail.com>
2017-04-30 23:32:34 -0500

    Add endpoints module

--- backend/__init__.py
+++ backend/__init__.py
@@ -41,5 +41,8 @@ migrate  = Migrate(app, db)
 # Importing the database models
 from backend import models
 
+# Importing the REST API
+from backend.api.v1 import endpoints
+
 # Importing the app views
 from backend import views
